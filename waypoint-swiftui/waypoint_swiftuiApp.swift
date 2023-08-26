decb2a9ed77992cb2f87dd1bc1bf1f79744f6132
Ankur Ahir <ahir.ankur0429@gmail.com>
2023-08-25 22:48:24 -0700

    Initial Commit

---
+++ waypoint-swiftui/waypoint_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  waypoint_swiftuiApp.swift
+//  waypoint-swiftui
+//
+//  Created by Ankur Ahir on 8/25/23.
+//
+
+import SwiftUI
+
+@main
+struct waypoint_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
